dde22cac986035c9fc19abb0b3356bbf78c45c7b
Olivier Nsabimana <onsabimana@gmail.com>
2024-04-14 10:00:03 +0200

    walking skeletons

---
+++ Reactivities/API/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt => {
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+}); // Add your DbContext here
+builder.Services.AddCors(opt => {
+    opt.AddPolicy("CorsPolicy", policy => {
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseCors("CorsPolicy");
+app.UseAuthorization();
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync(); // Apply any pending migrations
+    await Seed.SeedData(context); // Seed the database
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
+
+app.Run();
